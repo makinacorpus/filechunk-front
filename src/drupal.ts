87bd9fa1222baff9d9764b08407d8af4b2279b9c
Pierre Rineau <pierre.rineau@makina-corpus.com>
2018-03-14 15:34:24 +0100

    avoid bugguy javascript that may happen due to faulty drupal modules

--- src/drupal.ts
+++ src/drupal.ts
@@ -4,6 +4,10 @@ import { FilechunkWidget } from "./core";
 
 Drupal.behaviors.filechunk = {
     attach: (context: Element, settings: any) => {
+        if (!context.querySelectorAll) {
+            console.log(`Given context is not a valid HTML Element, some module are probably wrongly calling Drupal.attachBehavior() using a jQuery selector`);
+            return;
+        }
         for (let widget of <HTMLElement[]><any>context.querySelectorAll(".filechunk-widget-table")) {
             new FilechunkWidget(widget, Drupal.t);
         }
